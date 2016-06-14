fa7c4e157118caf57fdc574f854868072c2213ad
Dmitry_Alishchuk <ciliz.it@gmail.com>
2016-06-14 11:48:23 +0300

    ~~Asset and Resource constructors

---
+++ js/asset.js
@@ -0,0 +1,27 @@
+﻿function Asset(params) {
+    this.asset = new Image;
+    this.ready = false;
+    
+    if (params.src) {
+        this.asset.onload = this.onAssetReady.bind(this.asset, this);
+        this.asset.src = params.src;
+    }
+
+};
+
+Asset.prototype.onAssetReady = function (self, asset, width, height) {
+    self.ready = true;
+    self.width = this.naturalWidth;
+    self.height = this.naturalHeight;
+
+    if (self.readyListeners) {
+        for (var l = 0, ln = self.readyListeners.length; l < ln; l++) {
+            self.readyListeners[l](self);
+        };
+    };
+};
+
+Asset.prototype.onReady = function (fn) {
+    this.readyListeners = this.readyListeners || [];
+    this.readyListeners.push(fn);
+}
---
+++ js/resource.js
@@ -0,0 +1,26 @@
+﻿function Resource(params) {
+    this.renderable = document.createElement("canvas");
+    this.renderableContext = this.renderable.getContext("2d");
+    this.asset = params.asset;
+
+    if (this.asset) {
+        this.asset.onReady(function () {
+            this.init(this.asset, params.width, params.height);
+        }.bind(this));
+    };
+
+}
+
+Resource.prototype.init = function (asset, width, height) {
+    this.width = this.renderable.width = width || asset.width;
+    this.height = this.renderable.height = height || asset.height;
+    this.renderableContext.drawImage(asset.asset, 0, 0);
+};
+
+Resource.prototype.clone = function () {
+    return new Resource({
+        asset: this.asset,
+        width: this.width,
+        height: this.height
+    });
+};
